096fa85d8b03bee3283f295b32f62fab89c9cbed
PeterMagdyIskander <PeterMagdyIskander@gmail.com>
2022-05-08 20:43:42 +0200

    finally finished it

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "@/plugins/vuetify"; // path to vuetify export
+import { BootstrapVue } from "bootstrap-vue";
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  vuetify,
+  BootstrapVue,
+}).$mount("#app");
